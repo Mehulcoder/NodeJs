6fe7bf129ccdfbe3640ca9e36856054c05c99c0c
Mehulcoder <mehul355180@gmail.com>
2020-03-22 17:54:46 +0530

    Playing with Json

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,16 +3,32 @@ const chalk = require('chalk');
 var getNotes = require('./notes');
 const yargs = require('yargs');
 
+yargs.version('1.1.0');
+
 yargs.command({
     command: 'list',
-    description: 'List all the stuff',
-    builder
-    handler: function () {  
-        console.log("Listing all the stuff");
+    describe: 'List all the stuff',
+    builder:{
+        title:{
+            describe:'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+
+        body:{
+            describe:'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {  
+        console.log("Title: "+argv.title);
+        console.log("Body: "+argv.body);
     }
 })
 
-
+yargs.parse();
 // var command = process.argv[2];
 
-console.log(yargs.argv); 
+
+// console.log(yargs.argv); 
