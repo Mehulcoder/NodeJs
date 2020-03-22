1173e441a45d284c807c0cb28b5803d105fc6a58
Mehulcoder <mehul355180@gmail.com>
2020-03-22 20:52:28 +0530

    Added add note functionality

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,13 @@
-const validator = require('validator');
-const chalk = require('chalk');
-var getNotes = require('./notes');
-const yargs = require('yargs');
+var validator = require('validator');
+var chalk = require('chalk');
+var notes = require('./notes');
+var yargs = require('yargs');
 
 yargs.version('1.1.0');
 
 yargs.command({
-    command: 'list',
-    describe: 'List all the stuff',
+    command: 'add',
+    describe: 'Add note to the notes array',
     builder:{
         title:{
             describe:'Note title',
@@ -22,9 +22,8 @@ yargs.command({
         }
     },
     handler: function (argv) {  
-        console.log("Title: "+argv.title);
-        console.log("Body: "+argv.body);
+        notes.addNote(argv.title, argv.body);
     }
-})
+});
 
 yargs.parse();
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,9 +1,45 @@
+var fs = require('fs');
+
 function getNotes() {
     return "Your notes...";
 }
 
-var add notes = function () {  
+var addNote = function (title, body) {  
+    var notes = loadNodes();
+    var duplicateNotes = notes.filter(function (note) {  
+        return note.title === title
+    });
+
+    if (duplicateNotes.length===0) {
+        notes.push({
+            title:title,
+            body:body
+        });
+        console.log("New note added");
+        saveNotes(notes);
+    }else{
+        console.log("Duplicate!! Already exists")
+    }
     
+    
+};
+
+var saveNotes = function (notes) {  
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
 }
 
-module.exports = getNotes();
+var loadNodes = function () {
+    try {
+        var dataBuffer = fs.readFileSync('notes.json');
+        var dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch (e) {
+        return [];
+    }
+};
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+};
