26824e52c59cb74c2a83314d58477c7c93b36892
Mehulcoder <mehul355180@gmail.com>
2020-03-24 21:01:11 +0530

    Started the weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,8 @@
-console.log("Starting");
+var request = require('request');
 
+var url = 'https://api.darksky.net/forecast/3067f598a4ab75bcecd5b3c90ae80cd4/37.8267,-122.4233';
 
-
-console.log("Stopped");
+request({url:url, json:true}, (err, data)=>{
+    // var parsedData = JSON.parse(data.body);
+    console.log(data.body.currently);
+});
